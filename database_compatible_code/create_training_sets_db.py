6ede51c6296ea0269819b747a919f04cc6e2fa37
Ethan Dowley <34329672+edowley@users.noreply.github.com>
2023-01-31 16:35:29 +0800

    Changed default number of pulsars in training set

--- database_compatible_code/create_training_sets_db.py
+++ database_compatible_code/create_training_sets_db.py
@@ -32,8 +32,8 @@ import pandas as pd
 from urllib.parse import urljoin
 
 # Constants
-DEFAULT_NUM_PULSARS = 64
-DEFAULT_NUM_UNLABELLED = 256
+DEFAULT_NUM_PULSARS = 256
+DEFAULT_NUM_UNLABELLED = 512
 DEFAULT_VALIDATION_RATIO = 0.2
 SMART_BASE_URL = os.environ.get('SMART_BASE_URL', 'http://localhost:8000/')
 SMART_TOKEN = os.environ.get('SMART_TOKEN', 'fagkjfasbnlvasfdfwjf783YDF')
